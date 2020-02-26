2a6ba402a366f244bb90b78a807f2a9eb3885f59
liurui <liurui@daling.com>
2020-02-26 15:13:11 +0800

    1

--- src/test/java/com/liurui/AppTest.java
+++ src/test/java/com/liurui/AppTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
@@ -15,6 +19,22 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        List<Integer> a = new ArrayList<>();
+
+        a.add(1);
+        a.add(3);
+        a.add(6);
+
+        final Iterator<Integer> iterator = a.iterator();
+
+        while (iterator.hasNext()){
+            final Integer next = iterator.next();
+
+            if(next == 3){
+                iterator.remove();
+            }
+        }
+
+        System.out.println(a);
     }
 }
